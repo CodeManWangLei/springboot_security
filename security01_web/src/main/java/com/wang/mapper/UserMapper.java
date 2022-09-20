41d9deeca5a37d5240b2642c79d34bb97e3f00bc
lei wang <107055927+CodeManWangLei@users.noreply.github.com>
2022-09-20 15:12:08 +0800

    pull-test2

--- security01_web/src/main/java/com/wang/mapper/UserMapper.java
+++ security01_web/src/main/java/com/wang/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package com.wang.mapper;
 import com.wang.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
-
+// 命令行测试拉取
 @Mapper
 public interface UserMapper {
     User loadUserByUsername(String username);
