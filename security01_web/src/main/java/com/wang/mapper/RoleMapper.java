34fc710c40b55637e148d6c001f635f473d18ba8
lei wang <107055927+CodeManWangLei@users.noreply.github.com>
2022-09-20 15:10:05 +0800

    pull-test

--- security01_web/src/main/java/com/wang/mapper/RoleMapper.java
+++ security01_web/src/main/java/com/wang/mapper/RoleMapper.java
@@ -4,7 +4,7 @@ import com.wang.pojo.Role;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+// 在github修改了一下，试一试在本地能否拉取
 @Mapper
 public interface RoleMapper {
     List<Role> getRolesByUid(int uid);
